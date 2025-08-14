a285df2f0e045e0684640db1a12ecb8786984942
Paul Vreeswijk <pmvreeswijk@gmail.com>
2025-08-14 16:16:38 +0200

    small update to __init__.py

--- sip_tpv/__init__.py
+++ sip_tpv/__init__.py
@@ -1,7 +1,7 @@
 from .sip_to_pv import sip_to_pv
 from .pv_to_sip import pv_to_sip
 #import pkg_resources
-import importlib.metadata
+from importlib.metadata import version
 
 #__version__ = pkg_resources.get_distribution("sip_tpv").version
-__version__ = importlib.metadata.version("sip_tpv")
+__version__ = version("sip_tpv")
