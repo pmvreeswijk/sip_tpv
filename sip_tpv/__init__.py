4e48fdf5147ed0f18ae38a0df354ae50d73fc9bf
Paul Vreeswijk <pmvreeswijk@gmail.com>
2025-08-14 15:50:46 +0200

    replaced soon-obsolete pkg_resources with importlib.metadata

--- sip_tpv/__init__.py
+++ sip_tpv/__init__.py
@@ -1,5 +1,7 @@
 from .sip_to_pv import sip_to_pv
 from .pv_to_sip import pv_to_sip
-import pkg_resources
+#import pkg_resources
+import importlib.metadata
 
-__version__ = pkg_resources.get_distribution("sip_tpv").version
+#__version__ = pkg_resources.get_distribution("sip_tpv").version
+version = importlib.metadata.version("sip_tpv")
